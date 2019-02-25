e8d1587a17b3e2befeaa8d5caa1fca381df67941
Pietro Di Bello <pierodibello@gmail.com>
2019-02-25 22:52:58 +0100

    implement sigmoid function

--- coursera-machine-learning/machine-learning-ex2/ex2/plotData.m
+++ coursera-machine-learning/machine-learning-ex2/ex2/plotData.m
@@ -1,5 +1,5 @@
 function plotData(X, y)
-%PLOTDATA Plots the data points X and y into a new figure 
+%PLOTDATA Plots the data points X and y into a new figure
 %   PLOTDATA(x,y) plots the data points with + for the positive examples
 %   and o for the negative examples. X is assumed to be a Mx2 matrix.
 
@@ -12,7 +12,11 @@ figure; hold on;
 %               examples and 'ko' for the negative examples.
 %
 
+positives = find(y==1);
+negatives = find(y==0);
 
+plot(X(positives, 1), X(positives, 2), 'k+','LineWidth', 2, 'MarkerSize', 7);
+plot(X(negatives, 1), X(negatives, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7);
 
 
 
