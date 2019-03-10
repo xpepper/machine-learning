82725da1e5d2d1f2f77312c0498d0a7712195676
Pietro Di Bello <pierodibello@gmail.com>
2019-03-11 00:16:02 +0100

    add regularization after feature mapping

--- coursera-machine-learning/machine-learning-ex2/ex2/costFunctionReg.m
+++ coursera-machine-learning/machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,9 +17,12 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+h = sigmoid(X*theta);
+penalty = (lambda / (2*m)) * theta(2:end)' * theta(2:end);
+J = -1/m * (y' * log(h) + (1 - y)' * log(1 - h)) + penalty;
 
-
-
+grad(1) = 1/m * X(:,1)' * (h - y);
+grad(2:end) = 1/m * X(:,2:end)' * (h - y) + lambda/m * theta(2:end);
 
 
 % =============================================================
